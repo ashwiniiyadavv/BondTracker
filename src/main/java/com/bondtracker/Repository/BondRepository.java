0fcd9dbd47a77d69f0729a33e34d568298fc7cfd
root <root@AshwiniUbuntu1.yclg5fbfvqfuziv2y5n2evervg.gx.internal.cloudapp.net>
2025-01-30 07:15:34 +0000

    Intitial Setup and Code with Repository

---
+++ src/main/java/com/bondtracker/Repository/BondRepository.java
@@ -0,0 +1,12 @@
+package com.bondtracker.repository;
+
+import com.bondtracker.model.Bond;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BondRepository extends JpaRepository<Bond, Long> {
+
+    List<Bond> findByCustomerIdAndIsActive(String customerId, boolean isActive);
+
+}
---
+++ src/main/java/com/bondtracker/model/Bond.java
@@ -0,0 +1,61 @@
+package com.bondtracker.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Bond {
+
+    @Id
+    private Long bondId;
+    private String customerId;
+    private String bondName;
+    private LocalDate maturityDate;
+    private boolean isActive;
+
+    // Getters and Setters
+ public Long getBondId() {
+        return bondId;
+    }
+
+    public void setBondId(Long bondId) {
+        this.bondId = bondId;
+    }
+
+    // Getter and Setter for customerId
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    // Getter and Setter for bondName
+    public String getBondName() {
+        return bondName;
+    }
+
+    public void setBondName(String bondName) {
+        this.bondName = bondName;
+    }
+
+    // Getter and Setter for maturityDate
+    public LocalDate getMaturityDate() {
+        return maturityDate;
+    }
+
+    public void setMaturityDate(LocalDate maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+
+    // Getter and Setter for isActive
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
